320494a49924e7ce631c66545aa2d7a287aab7ab
P L <pownthep.dev@gmail.com>
2023-06-13 19:33:14 +0700

    ci: fix

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@ docker build \
   --tag=gcr.io/milk-platform-service/cloudrust:latest \
   --file=./Dockerfile \
   .
+
+docker run \
+  --interactive --tty \
+  --publish=8080:8080 \
+  --publish=8090:8090 \
+  --env=PORT="8080" \
+  gcr.io/milk-platform-service/cloudrust:latest
